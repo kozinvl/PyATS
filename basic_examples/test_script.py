dc2ac40b536f862355a71037631d1b8c6830631f
vkozin <vladiksystem@gmail.com>
2018-07-04 12:12:56 +0300

    Test case

---
+++ basic_examples/test_script.py
@@ -0,0 +1,36 @@
+import logging
+from ats import aetest
+
+log = logging.getLogger(__name__)
+
+
+class common_setup(aetest.CommonSetup):
+
+    @aetest.subsection
+    def subsection_one(self):
+        log.info('In Common Setup')
+        log.info('preparing execute tests')
+
+    @aetest.subsection
+    def subsection_two(self, section):
+        log.info('Doing something another')
+        log.info("inside %s" % (section))
+
+
+class Testcase_One(aetest.Testcase):
+
+    @aetest.setup
+    def setup(self):
+        log.info("%s Setup/Preparation" % self.uid)
+        self.a = 1
+        self.b = 2
+
+    @aetest.test
+    def test_one(self, section):
+        log.info("Execute Test section: %s in testcase %s" % (section.uid, self.uid))
+        assert self.a == 1
+
+    @aetest.test
+    def test_two(self, section):
+        log.info("Execute Test section: %s in testcase %s" % (section.uid, self.uid))
+        assert self.b == 2
