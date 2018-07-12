b2eec3e323f7cff2c487375358080baf2f72acaa
mshuma <mastershumakov@gmail.com>
2018-07-12 15:52:03 +0300

    Some changes

---
+++ case_1/first_task_easypy.py
@@ -0,0 +1,33 @@
+"""Example of pyATS work. Used just AETest package"""
+
+import logging
+from ats import aetest
+from ats.topology import loader
+
+log = logging.getLogger(__name__)
+log.info('Loading testbed from YAML file')
+testbed = loader.load("testbed_1.yaml")
+
+
+class Testcase_One(aetest.Testcase):
+    server = None
+
+    @aetest.setup
+    def setup(self):
+        log.info('Connection to Linux Server')
+        self.server = testbed.devices['vm-a']
+        self.server.connect()
+
+    @aetest.test
+    def test_one(self):
+        log.info('Check the results')
+        a = self.server.execute('hostname')
+        assert len(a) > 3
+
+    @aetest.cleanup
+    def cleanup(self):
+        log.info('Close connection to Server')
+
+
+if __name__ == '__main__':
+    aetest.main()
