bf6c2a345ac5ffadbb87bdebfcd5f96913798e64
vkozin <vladiksystem@gmail.com>
2018-08-01 14:40:59 +0300

    copy file from remote to local machine

--- case_3/README.md
+++ case_3/README.md
@@ -1,6 +1,11 @@
 Links:
 ======
-
 * [Multiprotocol File Transfer Utilities](https://pubhub.devnetcloud.com/media/pyats/docs/utilities/file_transfer_utilities.html)
 
 * [GenieFileTransferUtilsLibs](https://pubhub.devnetcloud.com/media/pyats-packages/docs/geniefiletransferutilslibs/introduction.html)
+
+* [Asynchronous Library](https://pubhub.devnetcloud.com/media/pyats/docs/async/introduction.html)
+
+* [Multiprocessing — Process-based parallelism](https://docs.python.org/3/library/multiprocessing.html)
+
+* [Asynchronous Methods](https://pubhub.devnetcloud.com/media/pyats/docs/async/versus.html)
--- case_3/data_copy_example.py
+++ case_3/data_copy_example.py
@@ -6,7 +6,10 @@ from genie.libs import filetransferutils
 # Transferring a single file to or from a remote server
 tb = loader.load("testbed.yaml")
 
-this_device = FileUtils.from_device(tb.devices['virtual_device'])
+this_device = FileUtils.from_device(tb.devices['remote_device'])
 
-this_device.copyfile(source='flash: ~/tmp/demo.txt',
-                     destination='tftp:/server_alias/path/to/file')
+this_device.copyfile(source='scp://remote_server:/tmp/demo.txt',
+                     destination='/Users/vkozin/Downloads/',
+                     timeout_seconds=15)
+
+assert this_device.is_local('/Users/vkozin/Downloads/demo.txt')
