4c0657451950425c5ea41ef56f6f7811a633c836
vkozin <vladiksystem@gmail.com>
2018-08-01 15:43:54 +0300

    copy file

--- case_3/data_copy_example.py
+++ case_3/data_copy_example.py
@@ -4,12 +4,19 @@ from ats.utils.fileutils import FileUtils
 from genie.libs import filetransferutils
 
 # Transferring a single file to or from a remote server
+
 tb = loader.load("testbed.yaml")
 
-this_device = FileUtils.from_device(tb.devices['remote_device'])
+# Instanciate a filetransferutils instance for the device corresponding
+# to the device specific OS
+this_device = FileUtils.from_device(tb.devices['my_device'])
 
+# copy from remote to local machine
 this_device.copyfile(source='scp://remote_server:/tmp/demo.txt',
                      destination='/Users/vkozin/Downloads/',
                      timeout_seconds=15)
 
-assert this_device.is_local('/Users/vkozin/Downloads/demo.txt')
+# copy from local to remote machine
+this_device.copyfile(source='/Users/vkozin/Downloads/Task_1.docx',
+                     destination='scp://remote_server:/tmp/',
+                     timeout_seconds=15)
